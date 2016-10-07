007051bdd59d9a30e15251c57e4ebea8fadc5a11
Sebastien Gadois <sgadois@gmail.com>
2016-10-07 11:52:25 +0200

    Reset du champs apres send de l'email

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ $(function(){
     $("#sendForm").click(function(e) {
         if($("#email").val()) {
             mixpanel.track($("#email").val());
+            $("#email").val('').placeholder();
         }
     });
     
