d450f05d5e13ad783001c0eee578e7638915018f
Sebastien Gadois <sgadois@gmail.com>
2016-10-12 10:48:34 +0200

    Ajout event pour taux conversion

--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
 $(function(){
+    function Generator() {};
+    Generator.prototype.rand =  Math.floor(Math.random() * 26) + Date.now();
+
+    Generator.prototype.getId = function() {
+        return this.rand++;
+    };
+    var idGen =new Generator();
+    
+    mixpanel.identify(idGen.getId());
+    mixpanel.track("Arrival");
+    
     $("#sendForm").click(function(e) {
         if($("#email").val()) {
-            mixpanel.identify($("#email").val());
             mixpanel.people.set({
                 "$created": new Date(),
                 "$email": $("#email").val()
