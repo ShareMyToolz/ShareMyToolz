4100d097a9eb585ab141c526d680f989aafc3ed9
Sebastien Gadois <sgadois@gmail.com>
2016-10-12 11:37:57 +0200

    Changement identifier mixpanel

--- js/script.js
+++ js/script.js
@@ -3,10 +3,9 @@ $(function(){
     
     $.get("http://ipinfo.io", function(response) {
         ip = response.ip;
+        mixpanel.identify(ip);
+        mixpanel.track("Arrival");
     }, "jsonp");
-    
-    mixpanel.identify(ip);
-    mixpanel.track("Arrival");
     
     $("#sendForm").click(function(e) {
         if($("#email").val()) {
